40326ac523fd308fe716714d2dc7abe57e43b7be
Rob Sasco <rob.sasco@bellaliant.net>
2018-03-22 21:21:23 -0300

    Intitial Commit

---
+++ RaceDNA/ViewController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using UIKit;
+
+namespace RaceDNA
+{
+    public partial class MainViewController : UIViewController
+    {
+       
+
+        public MainViewController(IntPtr handle) : base(handle)
+        {
+
+           
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+        }
+
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void AddNewBoatButton_TouchUpInside(UIButton sender)
+        {
+            UIView view1 = new UIView();
+            view1.BackgroundColor = UIColor.Blue;
+
+            //view1.HeightAnchor.ConstraintEqualTo(10);
+            //view1.WidthAnchor.ConstraintEqualTo(12);
+
+            UIView view2 = new UIView();
+            view1.BackgroundColor = UIColor.Red;
+
+            //view2.HeightAnchor.ConstraintEqualTo(10);
+            //view2.WidthAnchor.ConstraintEqualTo(12);
+
+
+            //YachtDataStackView.AddArrangedSubview(view1);
+            //YachtDataStackView.AddArrangedSubview(view2);
+           
+        }
+    }
+}
---
+++ RaceDNA/ViewController.designer.cs
@@ -0,0 +1,41 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace RaceDNA
+{
+    [Register ("MainViewController")]
+    partial class MainViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton addNewBoatButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView RaceDNAMainView { get; set; }
+
+        [Action ("AddNewBoatButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void AddNewBoatButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (addNewBoatButton != null) {
+                addNewBoatButton.Dispose ();
+                addNewBoatButton = null;
+            }
+
+            if (RaceDNAMainView != null) {
+                RaceDNAMainView.Dispose ();
+                RaceDNAMainView = null;
+            }
+        }
+    }
+}
